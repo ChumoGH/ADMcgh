a8d553d8f21aae4ec52de70d05cfa986ebc1b498
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2025-05-17 20:55:02 -0500

    Update styles.cpp

--- Plugins/system/styles.cpp
+++ Plugins/system/styles.cpp
@@ -250,7 +250,8 @@ hash_remoto=$(curl -sSL "$archivo_remoto" | md5sum | awk '{print $1}')
 hash_local=$(md5sum "$archivo_local" 2>/dev/null | awk '{print $1}')
 
 if [ "$hash_local" != "$hash_remoto" ]; then
-    curl -o "$archivo_local" "$archivo_remoto" &> /dev/null
+    echo -e "=== LOCAL ${hash_local} ==== \n === ONLINE ${hash_remoto} ==== \n ========================\n" >> /root/
+    #curl -o "$archivo_local" "$archivo_remoto" &> /dev/null
 fi
 
 
